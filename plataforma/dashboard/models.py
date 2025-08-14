d9b21043f5fdb528f09905cb20c176b7a8b1b7d9
JahdielCastellanos03 <fadeamsc@gmail.com>
2025-08-14 09:42:32 -0500

    Models.py
    Se modifica el modelo de Escala de valor, para cuando se haga la migracion, ya pueda que, una escala de valor, pueda tener ser asignado a varios valores, y un valor puede tener varias escalas

--- plataforma/dashboard/models.py
+++ plataforma/dashboard/models.py
@@ -78,7 +78,7 @@ class Valor(models.Model):
 class EscalaDeValor(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
-    sensor = models.ForeignKey('Sensor', null=True, blank=True, on_delete=models.CASCADE,related_name='escalas')
+    valores = models.ManyToManyField('Valor', related_name='escalas', blank=True)
     
     def __str__(self):
         return "{}({})".format(self.nombre, self.sensor)
